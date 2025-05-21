d777278d81f2e5b052c55eedb19d62d9bff1ac7a
vaasu2002 <vaasu.bisht2021@vitbhopal.ac.in>
2025-05-21 09:24:00 +0530

    feat(worker-node): implement worker heartbeat functionality and server setup

---
+++ health-system-monitor/worker-node/src/heart-beat.ts
@@ -0,0 +1,91 @@
+import axios from 'axios';
+
+const AXIOS_REQUEST_TIME_OUT = 5000;
+const HEARTBEAT_INTERVAL_MS = 4000
+export interface IWorkerHeartbeatConfig{
+    workerId: string;
+    port: string | number;
+    masterNodeUrl: string;
+    heartbeatIntervalMs?: number;
+}
+
+export class WorkerHeartbeat{
+    private readonly config: IWorkerHeartbeatConfig;
+    private heartbeatTimer: NodeJS.Timeout | null = null;
+    private readonly intervalMs: number;
+    private isRunning: boolean = false;
+    constructor(config: IWorkerHeartbeatConfig){
+        this.config = {
+            ...config,
+            heartbeatIntervalMs: config.heartbeatIntervalMs
+        };
+        this.intervalMs = this.config.heartbeatIntervalMs;
+    }
+
+
+    public async sendHeartBeat(): Promise<boolean>{
+        console.log(this.config)
+        try{
+            const response = await axios.post(
+                `${this.config.masterNodeUrl}/worker/heart-beat`, {
+                    config:this.config
+                },
+                {
+                    timeout:AXIOS_REQUEST_TIME_OUT
+                }
+            );
+            console.log(`successfully sent heartbeat to master node: ${JSON.stringify(response.data)}`);
+            return true;
+        }catch(error){
+            console.log(error)
+            if (axios.isAxiosError(error)){
+                console.error(`Failed to send heartbeat: ${error.message}`);
+                if(error.response){
+                    console.error(`Server responded with status: ${error.response.status}`);
+                } 
+                else if(error.request){
+                    console.error('No response received from server');
+                }
+            } 
+            else{
+                console.error(`Unexpected error sending heartbeat: ${error instanceof Error ? error.message : String(error)}`);
+            }
+            return false;
+        }
+    }
+
+    public start(): void{
+        if(this.isRunning){
+            console.log('Heartbeat service is already running');
+            return;
+        }
+        console.log(`Starting heartbeat interval every ${this.intervalMs}ms for worker ${this.config.workerId}`);
+        
+        try{
+            // send first heartbeat instantly...  we will continue even if first heart-beat fails
+            this.sendHeartBeat().catch(err => {
+                console.error('Error sending initial heartbeat:', err);
+            });
+
+            // imp: used arrow function to preserve 'this' context
+            this.heartbeatTimer = setInterval(() => {
+                this.sendHeartBeat().catch(err => {
+                    console.error('Error in heartbeat interval:', err);
+                });
+            }, HEARTBEAT_INTERVAL_MS);
+            this.isRunning = true;
+
+        }catch(error){
+            console.error(`Failed to start heartbeat service: ${error instanceof Error ? error.message : String(error)}`);
+            this.kill(); // distro(kill) process if we fails to start sending heart-beat
+        }
+    }
+
+    public isActive():boolean{
+        return this.isRunning;
+    }
+    public kill(exitCode: number = 1): void{
+        // process.kill(process.pid, 'SIGABRT');
+        // return process.exit(exitCode) as never;
+    }
+}
